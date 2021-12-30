1a4f1e0b8b7206cfdb5421bafc1d7f0effed2913
Yash Mardikar <yash.mardikar@gmail.com>
2021-12-30 16:29:21 -0700

    Time: 244 ms (16.79%), Space: 16.7 MB (18.93%) - LeetHub

--- single-number/single-number.py
+++ single-number/single-number.py
@@ -1,9 +1,15 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        cache=set()
+    def singleNumber1(self, nums: List[int]) -> int:
+        cache = set()
         for i in nums:
             if i in cache:
                 cache.remove(i)
             else:
                 cache.add(i)
         return cache.pop()
+    
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = nums[0]
+        for i in range(1, len(nums)):
+            xor = xor^nums[i]
+        return xor
