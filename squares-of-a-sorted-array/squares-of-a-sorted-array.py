baa1f7409cf4b338af3ff2b5fb88c5e58b46259c
Yash Mardikar <yash.mardikar@gmail.com>
2022-01-27 23:45:00 -0700

    Time: 200 ms, Memory: 16.2 MB - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -1,5 +1,6 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+        #nlogn
         new_nums=[]
         for i in range(len(nums)):
             new_nums.append(nums[i]*nums[i])
