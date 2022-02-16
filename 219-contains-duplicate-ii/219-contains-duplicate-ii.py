a54ebd5f3b6fee7b26cfb1d4b3a3dfe6108f0339
Yash Mardikar <yash.mardikar@gmail.com>
2022-02-16 03:58:21 -0700

    Time: 624 ms (77.45%), Space: 27.2 MB (68.41%) - LeetHub

--- 219-contains-duplicate-ii/219-contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/219-contains-duplicate-ii.py
@@ -1,12 +1,12 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         hmap = {}
-        for i, num in enumerate(nums):
+        for i in range(len(nums)):
+            num = nums[i]
             if num not in hmap:
                 hmap[num] = i
             elif abs(i-hmap[num]) <= k:
                     return True
             else:
-                #replace value, append not needed. Discard initial reapeat index.
                 hmap[num] = i
         return False
