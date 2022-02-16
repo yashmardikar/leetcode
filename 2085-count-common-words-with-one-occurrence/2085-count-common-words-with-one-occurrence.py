915cad424168b011cd3990491d9273969dd1dd09
Yash Mardikar <yash.mardikar@gmail.com>
2022-02-16 16:18:33 -0700

    Time: 98 ms (59.10%), Space: 14.5 MB (66.43%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
@@ -1,32 +1,11 @@
+from collections import Counter
 class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
-        
-        map1 = {}
-        #mapping word and their occurences
+        counter1 = Counter(words1)
+        counter2 = Counter(words2)
+        result = 0
         for word in words1:
-            if word not in map1:
-                map1[word] = 1
-            else:
-                map1[word] += 1
-        
-        #keeping single occurence words
-        new_words1 = []
-        for word, count in map1.items():
-            if count == 1:
-                new_words1.append(word)
-        
-        map2 = {}
-        #mapping word and their occurences
-        for word in words2:
-            if word not in map2:
-                map2[word] = 1
-            else:
-                map2[word] += 1
-        
-        #keeping single occurence words
-        new_words2 = []
-        for word, count in map2.items():
-            if count == 1:
-                new_words2.append(word)
+            if counter1[word] == 1 and counter2[word] == 1:
+                result += 1
+        return result
     
-        return len(set(new_words2).intersection(set(new_words1)))
