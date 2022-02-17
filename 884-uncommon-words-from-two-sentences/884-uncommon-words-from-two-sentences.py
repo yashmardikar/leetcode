ef4ef572e3bcf7efb8eaf76c5626bbddd99202ff
Yash Mardikar <yash.mardikar@gmail.com>
2022-02-16 19:45:24 -0700

    Time: 52 ms (25.71%), Space: 14 MB (63.30%) - LeetHub

--- 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.py
+++ 884-uncommon-words-from-two-sentences/884-uncommon-words-from-two-sentences.py
@@ -1,19 +1,11 @@
 from collections import Counter
 class Solution:
     def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
-        s1 = s1.split(" ")
-        s2 = s2.split(" ")
-        
-        counter1 = Counter(s1)    
-        counter2 = Counter(s2)
-        #print(counter1 + counter2)
-        uncommon_words = set(s1).symmetric_difference(set(s2))
-        remove_set = set()
-        for word in uncommon_words:
-            if counter1.get(word, 0) > 1 or counter2.get(word, 0) > 1:
-                remove_set.add(word)
-        
-        #print(counter1, counter2)
-        #print(uncommon_words, remove_set)
-        return uncommon_words - remove_set
-        
+        master_list = s1.split(" ") + s2.split(" ")
+        counter = Counter(master_list)
+        res_set = set()
+        for word, count in counter.items():
+            if count == 1:
+                res_set.add(word)
+            
+        return res_set
