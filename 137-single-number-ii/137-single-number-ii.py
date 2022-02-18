f9a32fb140269c4150d14c6654dd0fde048cfa67
Yash Mardikar <yash.mardikar@gmail.com>
2022-02-18 02:55:34 -0700

    Time: 85 ms (58.72%), Space: 15.9 MB (50.90%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,15 +1,4 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        visited = {}
-        #preprocessing arr in O(n)
-        for num in nums:
-            if num not in visited:
-                visited[num] = 1
-            else:
-                visited[num] += 1
-        
-        #check for num visted only once
-        for num, count in visited.items():
-            if count == 1:
-                return num
-        
+        return (3* sum(set(nums)) - sum(nums))//2
+            
