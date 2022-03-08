b9df03f73631364b1fc45c30653f905a14045fe8
Yash Mardikar <yash.mardikar@gmail.com>
2022-03-08 10:22:42 -0700

    Time: 89 ms (65.79%), Space: 15.1 MB (55.96%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,12 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        #range function from last index to first to keep first found index in map
-        maps = { target - nums[i] : i for i in range(len(nums))}
-        print(maps)
+        hmap = {}
         for i in range(len(nums)):
-            if nums[i] in maps:
-                #check if complement index and num index is different
-                if maps[nums[i]] != i:
-                    return maps[nums[i]],i
-        return -1
-            
+            if target - nums[i] in hmap:
+                return (i, hmap[target - nums[i]])
+            hmap[nums[i]] = i
