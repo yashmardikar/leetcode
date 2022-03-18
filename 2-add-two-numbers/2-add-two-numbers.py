c77362b72b23c37664741635fcab7475a0e94d1f
Yash Mardikar <yash.mardikar@gmail.com>
2022-03-17 23:24:32 -0700

    Time: 87 ms (62.69%), Space: 13.9 MB (91.18%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,28 +5,39 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        node1, node2 = l1, l2
-        head = new_node = ListNode()
-        carryOver = 0
-        while node1 or node2:
-            val1, val2 = 0, 0
-            if node1:
-                val1 = node1.val
-                node1 = node1.next
-            if node2:
-                val2 = node2.val
-                node2 = node2.next
-            val = val1 + val2 + carryOver
-            carryOver = val // 10
-            val = val % 10
-            new_node.val = val
-            if node1 or node2:
-                new_node.next = ListNode()
-                new_node = new_node.next
-            elif carryOver:
-                new_node.next = ListNode()
-                new_node.next.val = carryOver
-                new_node.next.next = None
-            else:
-                new_node.next = None
-        return head
+        l3 = node = ListNode() 
+        carry = 0
+        while l1 or l2:
+            #keep looping till both empty
+            val1 = 0
+            if l1:
+                #get curr number
+                val1 = l1.val
+                l1 = l1.next
+            
+            val2 = 0
+            if l2:
+                #get curr number
+                val2 = l2.val
+                l2 = l2.next
+            
+            currSum = val1 + val2 + carry
+            carry = currSum // 10
+            currSum = currSum % 10
+            #print(currSum, carry)
+            node.val = currSum
+            
+            prev = node
+            node.next = ListNode()
+            node = node.next
+            
+        if carry != 0:
+            #print('in')
+            node.val = carry
+            return l3
+        
+        if prev.next.val == 0:
+            prev.next = None
+        
+        return l3
+            
